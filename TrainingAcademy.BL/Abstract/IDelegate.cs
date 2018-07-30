65906afd3c3f43efeb1a99a4abe623523a9f19c8
Bonginkosi Mahlangu <BMahlangu@jhb.dvt.co.za>
2018-07-30 08:20:12 +0200

    Added delegate and registration CRUD methods

--- TrainingAcademy.BL/Abstract/IDelegate.cs
+++ TrainingAcademy.BL/Abstract/IDelegate.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainingAcademy.DAL;
+using TrainingAcademy.UI.ViewModel;
 
 namespace TrainingAcademy.BL.Abstract
 {
     public interface IDelegate
     {
+        void AddDelegate(DAL.Delegate @delegate);
+        List<Dietary> GetDietaries();
+        List<DAL.Delegate> ListDelegate();
+        void RemoveDelegate(int id);
+        DAL.Delegate UpdateDelegate(int id);
+        DAL.Delegate GetDelegate(int Id);
+
     }
 }
